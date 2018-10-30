ffdf8371951a52b76526dca0d299a2e70bf7880b
Nelson Nnaji <nnaji_udochukwu@yahoo.com>
2018-10-30 10:20:12 +0100

    modify webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html'
+      template: path.resolve(__dirname, './public/index.html')
     })
   ]
 };
