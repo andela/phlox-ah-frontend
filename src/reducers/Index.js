84b4f97ccee7402667e43ac9fdcb9c404fc9c316
Runor Adjekpiyede <kampkellykeys@gmail.com>
2018-11-07 11:14:45 +0100

    feature(reset-password): delete demoArticleReducer
    This commit deletes the ‘demoArticleReducer’.
    
    Finishes #160451455

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import demoArticleReducer from './DemoArticleReducer';
 import userReducer from './UserReducer';
 import msgInfoReducer from './MsgInfoReducer';
 import passwordReducer from './PasswordReducer';
 
 const rootReducer = combineReducers({
-  demoArticleReducer,
   passwordReducer,
   User: userReducer,
   Info: msgInfoReducer
