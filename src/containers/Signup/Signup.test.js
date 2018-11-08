f4117707fb1e9b4697a21680db68b595d2ab7ee5
Nelson Nnaji <nnaji_udochukwu@yahoo.com>
2018-11-08 01:54:59 +0100

    write tests for signup

---
+++ src/containers/Signup/Signup.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+
+import Signup from './Signup';
+
+const mockStore = configureMockStore();
+const store = mockStore({
+  Signup: {
+    loading: true,
+    success: false,
+    failure: false,
+    errors: []
+  }
+});
+
+let component;
+let myComponent;
+
+describe('<Signup/>', () => {
+  beforeEach(() => {
+    component = shallow(
+      <Provider store={store}>
+        <Signup />
+      </Provider>
+    );
+    myComponent = component.dive({ context: { store } }).dive();
+  });
+  it('should render without throwing an error', () => {
+    expect(component).toMatchSnapshot();
+  });
+  it('should have a form tag with class col', () => {
+    expect(myComponent.find('form').hasClass('col')).toBe(true);
+  });
+  it('should have a signup button', () => {
+    expect(myComponent.find('Button#signupButton').exists()).toBe(true);
+  });
+  it('should have a input field', () => {
+    expect(myComponent.find('Input').exists()).toBe(true);
+  });
+});
