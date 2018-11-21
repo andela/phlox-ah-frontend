d8931517ec763feb0a2f6d237078df9bb2d1cafc
Onwuzor Victor <onwuzorvictor.com>
2018-11-21 10:36:17 +0100

    fix merge conflict

--- src/containers/ViewArticle/ViewArticle.test.js
+++ src/containers/ViewArticle/ViewArticle.test.js
@@ -5,7 +5,7 @@ import configureMockStore from 'redux-mock-store';
 import ViewArticle from './ViewArticle';
 import { articleStore } from './mockStore';
 import { asyncActions } from '../BasePath';
-import { RATE_ARTICLE, GET_ALL_COMMENT } from '../../actionTypes';
+import { RATE_ARTICLE, GET_ALL_COMMENT, CREATE_COMMENT } from '../../actionTypes';
 
 const mockStore = configureMockStore();
 const store = mockStore(articleStore);
@@ -41,5 +41,15 @@ describe('<ViewArticle/>', () => {
       payload
     };
     expect(asyncActions(GET_ALL_COMMENT).success(payload)).toEqual(expectedAction);
+  });
+  it('should dispatch an action to create a comment', () => {
+    const payload = {
+      articleSlug: 'title-of-article2'
+    };
+    const expectedAction = {
+      type: 'CREATE_COMMENT_SUCCESS',
+      payload
+    };
+    expect(asyncActions(CREATE_COMMENT).success(payload)).toEqual(expectedAction);
   });
 });
