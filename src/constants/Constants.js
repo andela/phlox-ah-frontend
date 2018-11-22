500c8ffb474ab46e7d35023c241efaee5f984ce4
Onwuzor Victor <onwuzorvictor.com>
2018-11-22 10:40:46 +0100

    feature(article-comment): add more test case for articles comment

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,6 +1,4 @@
-// const BASE_URL = 'https://phlox-ah-staging.herokuapp.com';
-
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://phlox-ah-staging.herokuapp.com';
 
 export const articleConstant = {
   ALL_ARTICLES_URL: `${BASE_URL}/api/v1/articles/feed`,
